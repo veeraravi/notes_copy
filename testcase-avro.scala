d56fd1ca41d9ace57dc864f27e37b61603a0630a
Veeraravi Kumar Singiri <veeraravikumar.singiri@gmail.com>
2024-10-22 16:04:15 -0500

    Update testcase-avro.scala

--- testcase-avro.scala
+++ testcase-avro.scala
@@ -18,56 +18,16 @@ class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
     val schemaSubjectId = "-value"
     val topicValueName = inputTopic + schemaSubjectId
 
-    // Mock the RestService response to return a schema
-    val mockRestResponseSchema = new RestSchema(topicValueName, 1, 1, "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"field1\",\"type\":\"string\"}]}", "AVRO")
-
+    // Mock the RestSchema to return a schema string when getSchema() is called
+    val mockRestResponseSchema = mock(classOf[RestSchema])
+    val avroSchemaString = "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"field1\",\"type\":\"string\"}]}"
+    
+    // Ensure that mockRestResponseSchema.getSchema returns the Avro schema string
     when(mockRestService.getLatestVersion(anyString())).thenReturn(mockRestResponseSchema)
+    when(mockRestResponseSchema.getSchema).thenReturn(avroSchemaString)
 
-    // Mock the Avro Parser to parse the schema
-    val mockAvroSchema = new Parser().parse(mockRestResponseSchema.getSchema)
-
-    // Mock SchemaConverters.toSqlType to return a StructType
-    val mockStructType = mock(classOf[StructType])
-    when(SchemaConverters.toSqlType(mockAvroSchema).dataType.asInstanceOf[StructType]).thenReturn(mockStructType)
-
-    // Act
-    val schema = getSchemaFromSchemaRegistry(mockRestService, inputTopic)
-
-    // Assert
-    schema shouldBe a[StructType]
-    // Additional assertions for fields
-    schema.fieldNames should contain allOf("field1")
-  }
-}
-
---------------------------------------------
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-import org.mockito.Mockito._
-import org.mockito.ArgumentMatchers._
-import io.confluent.kafka.schemaregistry.client.rest.RestService
-import io.confluent.kafka.schemaregistry.client.rest.entities.Schema as RestSchema
-import org.apache.avro.Schema.Parser
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.avro.SchemaConverters
-import org.apache.avro.Schema
-
-class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
-
-  test("getSchemaFromSchemaRegistry should return the correct StructType schema") {
-    // Arrange
-    val mockRestService = mock(classOf[RestService], RETURNS_DEEP_STUBS)
-    val inputTopic = "test-topic"
-    val schemaSubjectId = "-value"
-    val topicValueName = inputTopic + schemaSubjectId
-
-    // Mock the RestService response to return a schema
-    val mockRestResponseSchema = new RestSchema(topicValueName, 1, 1, "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"field1\",\"type\":\"string\"}]}", "AVRO")
-
-    when(mockRestService.getLatestVersion(anyString())).thenReturn(mockRestResponseSchema)
-
-    // Mock the Avro Parser to parse the schema
-    val mockAvroSchema = new Parser().parse(mockRestResponseSchema.getSchema)
+    // Mock the Avro Parser to parse the schema string
+    val mockAvroSchema = new Parser().parse(avroSchemaString)
 
     // Mock SchemaConverters.toSqlType to return a StructType
     val mockStructType = mock(classOf[StructType])
@@ -82,65 +42,3 @@ class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
     schema.fieldNames should contain ("field1")
   }
 }
-
-------------------------------------------------------
-
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-import org.mockito.Mockito._
-import org.mockito.ArgumentMatchers._
-import io.confluent.kafka.schemaregistry.client.rest.RestService
-import org.apache.avro.Schema
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.avro.SchemaConverters
-import org.mockito.Mockito.{RETURNS_DEEP_STUBS, mock}
-
-class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
-
-  test("getSchemaFromSchemaRegistry should return the correct StructType schema") {
-    // Arrange
-    val mockRestService = mock(classOf[RestService], RETURNS_DEEP_STUBS)
-    val inputTopic = "test-topic"
-    val schemaSubjectId = "-value"
-    val topicValueName = inputTopic + schemaSubjectId
-
-    // Mock response schema as JSON string
-    val schemaJson = "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"field1\",\"type\":\"string\"}]}"
-    
-    // Mock the RestService to return a mocked schema response
-    when(mockRestService.getLatestVersion(anyString())).thenReturn({
-      val mockSchema = mock(classOf[io.confluent.kafka.schemaregistry.client.rest.entities.Schema])
-      when(mockSchema.getSchema).thenReturn(schemaJson)
-      mockSchema
-    })
-
-    // Use Avro Schema Parser to parse the mocked schema JSON
-    val avroSchema = new Schema.Parser().parse(schemaJson)
-
-    // Mock SchemaConverters to convert Avro Schema to StructType
-    val mockStructType = mock(classOf[StructType])
-    when(SchemaConverters.toSqlType(avroSchema).dataType.asInstanceOf[StructType]).thenReturn(mockStructType)
-
-    // Act
-    val schema = getSchemaFromSchemaRegistry(mockRestService, inputTopic)
-
-    // Assert
-    schema shouldBe a[StructType]
-    // Verify the field names (this assumes mockStructType behaves as expected)
-    schema.fieldNames should contain ("field1")
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
