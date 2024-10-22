f1bb56cdba812c8132b6bd52e4dc2c843bd5fd66
Veeraravi Kumar Singiri <veeraravikumar.singiri@gmail.com>
2024-10-22 15:22:55 -0500

    Update testcase-avro.scala

--- testcase-avro.scala
+++ testcase-avro.scala
@@ -83,6 +83,53 @@ class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
   }
 }
 
+------------------------------------------------------
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.mockito.Mockito._
+import org.mockito.ArgumentMatchers._
+import io.confluent.kafka.schemaregistry.client.rest.RestService
+import org.apache.avro.Schema
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.avro.SchemaConverters
+import org.mockito.Mockito.{RETURNS_DEEP_STUBS, mock}
+
+class SchemaRegistryFunSuite extends AnyFunSuite with Matchers {
+
+  test("getSchemaFromSchemaRegistry should return the correct StructType schema") {
+    // Arrange
+    val mockRestService = mock(classOf[RestService], RETURNS_DEEP_STUBS)
+    val inputTopic = "test-topic"
+    val schemaSubjectId = "-value"
+    val topicValueName = inputTopic + schemaSubjectId
+
+    // Mock response schema as JSON string
+    val schemaJson = "{\"type\":\"record\",\"name\":\"Test\",\"fields\":[{\"name\":\"field1\",\"type\":\"string\"}]}"
+    
+    // Mock the RestService to return a mocked schema response
+    when(mockRestService.getLatestVersion(anyString())).thenReturn({
+      val mockSchema = mock(classOf[io.confluent.kafka.schemaregistry.client.rest.entities.Schema])
+      when(mockSchema.getSchema).thenReturn(schemaJson)
+      mockSchema
+    })
+
+    // Use Avro Schema Parser to parse the mocked schema JSON
+    val avroSchema = new Schema.Parser().parse(schemaJson)
+
+    // Mock SchemaConverters to convert Avro Schema to StructType
+    val mockStructType = mock(classOf[StructType])
+    when(SchemaConverters.toSqlType(avroSchema).dataType.asInstanceOf[StructType]).thenReturn(mockStructType)
+
+    // Act
+    val schema = getSchemaFromSchemaRegistry(mockRestService, inputTopic)
+
+    // Assert
+    schema shouldBe a[StructType]
+    // Verify the field names (this assumes mockStructType behaves as expected)
+    schema.fieldNames should contain ("field1")
+  }
+}
 
 
 
